6df18c5ee08afdde336d1b706f6b356960f1afef
franzero01 <frankzero01@gmail.com>
2018-07-18 13:14:19 -0400

    start

---
+++ src/gtnw/country.java
@@ -0,0 +1,11 @@
+package gtnw;
+
+public class country {
+	String name;
+	int cities;
+	int populaiton;
+	enum language{
+		Russian,
+		English
+	}
+}
---
+++ src/gtnw/seed.java
@@ -0,0 +1,10 @@
+package gtnw;
+
+public class seed {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+	}
+
+}
